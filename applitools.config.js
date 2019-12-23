bcf7087860dfc692cfc7b5452a4b57107f33d887
rivkabeck52 <rivka.beck@applitools.com>
2019-12-23 11:52:45 +0200

    first commit

---
+++ applitools.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    appName: 'Circle CI',
+    showLogs: true,
+    batchName: 'Circle CI batch'
+    // all other configuration variables apply
+  }
---
+++ cypress/integration/examples/actions.spec.js
@@ -0,0 +1,18 @@
+describe('Hello world', () => {
+  beforeEach(() => {
+    cy.eyesOpen({
+      browser: { width: 800, height: 600 },
+    });
+  });
+
+  afterEach(() => {
+    cy.eyesClose();
+  });
+
+  it('Demo Applitools Standard', () => {
+    cy.visit('https://demo.applitools.com/');
+    cy.eyesCheckWindow('Main Page');
+    // cy.get('button').click();
+    // cy.eyesCheckWindow('Click!');
+  });
+});
